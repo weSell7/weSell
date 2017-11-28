89032acf5bf10680cdaa7c46dc5870c6b1c595c0
yungz7 <wyungz7@gmail.com>
2017-11-28 11:34:05 +0800

    Revert "add link (logo)"
    This reverts commit aaa12c31fd9dcc679fa297aa1674c648e3a989ad.

--- computer.php
+++ computer.php
@@ -84,7 +84,7 @@
             <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="homepage.php"><label style="font-size: 50px;">weSell</label></a>
+                <a class="navbar-brand" href="#"><label style="font-size: 50px;">weSell</label></a>
             </div>
             <br>
             <input style="font-size: 20;" type="text" id="input" onkeyup="search()" placeholder="I'm looking for...">
