690720c25e87c432fe134ab52ac49f0e584f929d
yungz7 <wyungz7@gmail.com>
2017-11-28 11:40:58 +0800

    Revert "add tag"
    This reverts commit 40a259a9be567045ac648fb2a776847261fe04ee.

--- kitchen.php
+++ kitchen.php
@@ -120,7 +120,7 @@
 			</form>
 		</div>
 
-		<center><h1><marquee><text style='color: red;'> SALE!! Up to 30% Discount on selected products! </text></marquee></h1></center>;
+		<center><h1><text><text style='color: red;'> SALE!! Up to 30% Discount on selected products! </text></h1></center>;
 		
 		<center>
 			<table id="table1" style="border: solid; width: 80%; height: 70%;">
