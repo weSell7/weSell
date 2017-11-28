d54f62e1c4feeca725652a3644dddc73fd7b8f38
MaxClark1 <chunyungz@gmail.com>
2017-11-28 20:17:14 +0800

    put navbar in register page

--- register/registerform.php
+++ register/registerform.php
@@ -5,10 +5,37 @@
 <!DOCTYPE html>
 <html>
 <head>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
 	<title>weSell | Registration</title>
 </head>
 <body style="background-image: url('../img/background2.jpg');">
-  	<a href="..\homepage.php"><img src="..\logo\logo3.jpg" /></a>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div style="background-color: #0D314B;"class="container-fluid">
+        <div style="background-color: #0D314B;"class="navbar-header">
+            <a class="navbar-brand" href="#"><label style="font-size: 50px;">weSell</label><span class="glyphicon glyphicon-shopping-cart"></span></a>
+        </div>
+        <ul class="nav navbar-nav">
+        <li><a href="..\homepage.php">Home</a></li>
+           
+        </ul>
+            
+         <!--Time-->
+            <div align="right">
+            <script type="text/javascript">    
+			    var date = new Date();
+			    document.writeln("<label style='color: white;'>"+date.toDateString()+"</label>");
+		    </script>
+            </div>
+        </div>
+    </nav>
+    
+    <br><br><br><br>
+
 	<center>
 
 		<?php 
