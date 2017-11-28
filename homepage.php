8c7fbf19a373949c01181a4537b1a1cef18f50b2
MaxClark1 <chunyungz@gmail.com>
2017-11-28 15:38:38 +0800

    update homepage

--- homepage.php
+++ homepage.php
@@ -23,18 +23,11 @@
     	</style>
 	</head>
 	<body style="background-image: url('img/background2.jpg');">
-		
-		
-        <br><br><br><br><br><br><br><br>    
+
+        <br><br><br><br><br><br><br>    
 		
 		<center><h1>Welcome To weSell</h1></center>
-
-		
-		<p style="background: lightblue;">
-		
-		</p>
-		
-		<br>
+        
 		<center>
             
         <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -76,137 +69,166 @@
   		
 		<br>
 		</center>
+        
 		
 		<!-- Add Slideshow-->
 		<div align="left">
-		<center><table id="table1" style="border: blue ; width: 60%; height: 50%;">
-				<tr>
-					<td>
+		<center>
+        <table id="table1" style="border: blue ; width: 60%; height: 50%;">
+        <tr>
+        <td>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
+        <style>
 		* {box-sizing:border-box}
 		body {font-family: Verdana,sans-serif;}
 		.mySlides {display:none}
 
 		/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
+        .slideshow-container {
+        max-width: 1000px;
+        position: relative;
+        margin: auto;
+        }
 
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
+        /* Caption text */
+        .text {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+        }
 
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
+        /* Number text (1/3 etc) */
+        .numbertext {
+        color: #f2f2f2;
+        font-size: 12px;
+        padding: 8px 12px;
+        position: absolute;
+        top: 0;
+        }
 
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
+        /* The dots/bullets/indicators */
+        .dot {
+        height: 15px;
+        width: 15px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+        }
 
-.active {
-  background-color: #717171;
-}
+        .active {
+        background-color: #717171;
+        }
 
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
+        /* Fading animation */
+        .fade {
+        -webkit-animation-name: fade;
+        -webkit-animation-duration: 1.5s;
+        animation-name: fade;
+        animation-duration: 1.5s;
+        }
 
-@-webkit-keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
+        @-webkit-keyframes fade {
+        from {opacity: .4} 
+        to {opacity: 1}
+        }
 
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
+        @keyframes fade {
+        from {opacity: .4} 
+        to {opacity: 1}
+        }
 
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
-</style>
+        /* On smaller screens, decrease text size */
+        @media only screen and (max-width: 300px) {
+        .text {font-size: 11px}
+        }
+        </style>
 
-<div class="slideshow-container"> 
+        <div class="slideshow-container"> 
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="slideshow\rphone.png" style="width:100%">
+            </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="slideshow\rphone.png" style="width:100%">
-</div>
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="slideshow\computer1.jpg" style="width:100%">
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="slideshow\computer1.jpg" style="width:100%">
-</div>
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="slideshow\shirt1.jpg" style="width:100%">
+        </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="slideshow\shirt1.jpg" style="width:100%">
-</div>
+        </div><br>
 
-</div>
-<br>
+        <div style="text-align:center">
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+        </div>
 
-<div style="text-align:center">
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-</div>
+        <script>
+            var slideIndex = 0;
+            showSlides();
 
-<script>
-var slideIndex = 0;
-showSlides();
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                var dots = document.getElementsByClassName("dot");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";  
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {slideIndex = 1}    
+                    for (i = 0; i < dots.length; i++) {
+                        dots[i].className = dots[i].className.replace(" active", "");
+                    }
+                slides[slideIndex-1].style.display = "block";  
+                dots[slideIndex-1].className += " active";
+                setTimeout(showSlides, 3000); // Change image every 3 seconds
+            }
+        </script>
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
-</script>
-
-<h4 class="unit-header">Your one-stop online store</h4>
-<h5 class="unit-subheader">weSell online shop offers you variety of choices from fashion to computer equipment that will satisfy your needs without hurting your wallet. We never compromise the quality of the products because we make sure the suppliers are from trusted stores. </h5>				
-					</td>
-				</tr>
-            </table></center></div>
+        <h4 class="unit-header">Your one-stop online store</h4>
+        <h5 class="unit-subheader">weSell online shop offers you variety of choices from fashion to computer equipment that will satisfy your needs without hurting your wallet. We never compromise the quality of the products because we make sure the suppliers are from trusted stores. </h5>				
+        </td>
+        </tr>
+        </table></center></div><br>
+        
+        <!--Box-->
+        <center>
+        <div class="container">    
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="panel panel-primary" style="background-color:#0D314B;">
+                        <div class="panel-heading">YEAR END SALES</div>
+                        <div class="panel-body"><img src="img/hpage2.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+                        <div class="panel-footer">Crazy Sale is finally here!</div>
+                    </div>
+                </div>
+            <div class="col-sm-4"> 
+                <div class="panel panel-primary" style="background-color:#0D314B;">
+                    <div class="panel-heading">COMING SOON!</div>
+                    <div class="panel-body"><img src="img/hpage.jpg" class="img-responsive" style="width:100%; height:100%;" alt="Image"></div>
+                    <div class="panel-footer">Don't Miss 12/12 Fever Sales!</div>
+                </div>
+            </div>
+            <div class="col-sm-4"> 
+                <div class="panel panel-primary" style="background-color:#0D314B;">
+                    <div class="panel-heading">Christmas DEAL</div>
+                    <div class="panel-body"><img src="img/hpage3.jpg" class="img-responsive" style="width:100%; height:100%;" alt="Image"></div>
+                    <div class="panel-footer">Up to 80% off Free Shipping Above RM80</div>
+                </div>
+            </div>
+            </div>
+        </div><br>
+        </center>
 
 		<p>&copy; weSell 2017</p>
 	</body>
